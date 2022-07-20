2efa4ebaae10f7f91b816f0123f4aeff1d4e51dd
Khanhnguyenvan150697 <Duongminh150697@gmail.com>
2022-07-20 23:37:40 +0700

    Route and MVC

--- NodeJS/Learning_Nodejs/src/index.js
+++ NodeJS/Learning_Nodejs/src/index.js
@@ -4,6 +4,7 @@ const {engine} = require('express-handlebars')
 const app = express()
 const port = 3000
 const morgan = require('morgan')
+const route = require('./routes')
 
 app.use(express.static(path.join(__dirname,'public')))
 
@@ -21,22 +22,8 @@ app.engine('hbs',engine({extname: '.hbs', defaultLayout: 'main'}))
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname,'resources\\views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  res.render('news');
-})
-
-app.get('/search', (req, res) => {
-  res.render('search');
-})
-
-app.post('/search', (req, res) => {
-  console.log(req.body)
-  res.render('search');
-})
+//route init
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
