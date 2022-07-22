8a24a19c8c889b4fd0733031ed9aad44d2c472d7
Khanhnv105 <duongminh15061997@gmail.com>
2022-07-22 21:21:30 +0700

    fix route

--- NodeJS/Learning_Nodejs/src/index.js
+++ NodeJS/Learning_Nodejs/src/index.js
@@ -24,11 +24,11 @@ app.use(morgan('combined'))
 //handlebars template engine
 app.engine('hbs',engine({extname: '.hbs', defaultLayout: 'main'}))
 app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname,'resources\\views'))
+app.set('views', path.join(__dirname,'resources','views'))
 
 //route init
 route(app)
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
